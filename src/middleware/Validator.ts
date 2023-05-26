97301d5007a1e92dbb182879f3b9b245207ad43b
sanaullah <sanaullahhaifz@gmail.com>
2023-05-26 22:31:16 +0400

    added profile update route

--- src/middleware/Validator.ts
+++ src/middleware/Validator.ts
@@ -46,6 +46,22 @@ export const Schema = {
         "string.empty": "Password is required.",
       }),
     }),
+    // Validation for Update User Profile route
+    profile: Joi.object<IUser>({
+      name: Joi.string().max(20).required().messages({
+        "string.empty": "Name is required.",
+        "string.max": "Name must be of 20 characters max.",
+      }),
+      email: Joi.string().email().required().messages({
+        "string.empty": "Email is required.",
+        "string.email": "Email is invalid.",
+      }),
+
+      propic: Joi.number().max(10).required().messages({
+        "number.empty": "Profile ico is required.",
+        "number.max": "Profile ico must less than 10.",
+      }),
+    }),
   },
 
   Watchlist: {
--- src/routes/User.ts
+++ src/routes/User.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { Schema, Validator } from "../middleware/Validator";
 import { authenticate } from "../middleware/auth";
 import {
@@ -8,11 +8,31 @@ import {
   signin,
   signup,
 } from "../controllers/User";
+import User from "../models/User";
 
 const router = express.Router();
 
 router.post("/signin", Validator(Schema.User.signin), signin);
 router.post("/signup", Validator(Schema.User.signup), signup);
+
+router.post(
+  "/profile",
+  authenticate,
+  Validator(Schema.User.profile),
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { name, email, propic } = req.body;
+      const { user } = req;
+
+      user?.set({ name, email, propic });
+      await user?.save();
+
+      return res.status(200).json({ message: "Profile updated successfully." });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 router.get("/me", authenticate, sendUserProfile);
 router.delete("/me", authenticate, deleteAccount);
