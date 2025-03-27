28a949dc5d6e1f53cf7692cf921950fab995d430
sanaullah <sanaullahhaifz@gmail.com>
2025-03-27 20:48:15 +0400

    mongodb move to local

--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,8 @@ app.use(
       "https://flixtr-fe.vercel.com",
       "http://localhost:3001",
       "http://localhost:3000",
+      "http://localhost:6000",
+      "http://localhost:6001",
       "http://164.152.26.217",
     ],
     credentials: true,
